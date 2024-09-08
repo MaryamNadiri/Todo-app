b271559960fd490c56472e8796872fecf206819d
Maryam <Nadiri.maryam@gmail.com>
2024-09-08 23:25:14 +0330

    3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const editButton = document.getElementById("edit-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody");
 const deleteAllButton = document.getElementById("delete-all-button");
@@ -30,23 +31,26 @@ const showAlert = (message, type) => {
 };
 
 const displayTodos = () => {
-    todosBody.innerHTML ="";
+  todosBody.innerHTML = "";
   if (!todos.length) {
     todosBody.innerHTML = "<tr><td colspan='4'>No task found!</td></tr>";
     return;
   }
 
   todos.forEach((todo) => {
-    todosBody.innerHTML +=
-      `
+    todosBody.innerHTML += `
             <tr>
                 <td>${todo.task}</td>
                 <td>${todo.date || "No Date"}</td>
                 <td>${todo.completed ? "completed" : "pending"}</td>
                 <td>
-                    <button>Edit</button>
-                    <button>Do</button>
-                    <button>Delete</button>
+                    <button onclick="editHandler('${todo.id}')">Edit</button>
+                    <button onclick="toggleHandler('${todo.id}')">
+                      ${todo.completed ? "Undo" : "Do"}
+                    </button>
+                    <button onclick="deleteHandler('${
+                      todo.id
+                    }')">Delete</button>
                 </td>
              </tr>
          `;
@@ -76,16 +80,56 @@ const addHandler = () => {
 };
 
 const deleteAllHandler = () => {
-    if (todos.length) {
-    todos = []; 
+  if (todos.length) {
+    todos = [];
     saveToLocalStorage();
     displayTodos();
-    showAlert("All todos deleted successfully" , "success");
-    } else {
-        showAlert ("No todos to clear" ,"error")
-    }
+    showAlert("All todos deleted successfully", "success");
+  } else {
+    showAlert("No todos to clear", "error");
+  }
+};
+
+const deleteHandler = (id) => {
+  const newTodos = todos.filter((todo) => todo.id !== id);
+  todos = newTodos;
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo deleted successfully", "success");
+};
+
+const toggleHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo status changed successfully", "success");
+};
+
+const editHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addButton.style.display = "none";
+  editButton.style.display = "inline-block";
+  editButton.dataset.id = id;
+};
+
+const applyEditHandler = (event) => {
+  const id = event.target.dataset.id;
+  const todo = todos.find((todo) => todo.id === id);
+  todo.task = taskInput.value;
+  todo.date = dateInput.value;
+  taskInput.value = "";
+  dateInput.value = "";
+  addButton.style.display = "inline-block";
+  editButton.style.display = "none";
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo edited successfully", "success");
 };
 
 window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
-deleteAllButton.addEventListener("click", deleteAllHandler)
+deleteAllButton.addEventListener("click", deleteAllHandler);
+editButton.addEventListener("click", applyEditHandler);
